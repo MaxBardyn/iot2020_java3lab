00d066a1ea87b540dc7bfa5e7ddc7278f3ff1106
MaxBardyn <maxbardyn@gmail.com>
2020-03-31 16:31:37 +0300

    add 4 different sorting methods and some tests

--- src/main/java/ua/lviv/iot/clothesStore/manager/ClothesManagerUtils.java
+++ src/main/java/ua/lviv/iot/clothesStore/manager/ClothesManagerUtils.java
@@ -1,5 +1,6 @@
 package ua.lviv.iot.clothesStore.manager;
 
+import java.util.Comparator;
 import java.util.List;
 
 import ua.lviv.iot.clothesStore.model.AbstractClothes;
@@ -8,10 +9,46 @@ import ua.lviv.iot.clothesStore.model.SortType;
 public class ClothesManagerUtils {
 
   public static void sortClothesByBrands(List<AbstractClothes> clothes, SortType sortType) {
+    clothes.sort(sortType == SortType.ASC ? new SortClothesByBrands() : new SortClothesByBrands().reversed());
+  }
+
+  public static void sortClothesByPriceInUAH(List<AbstractClothes> clothes, SortType sortType) {
+    ClothesManagerUtils sorter = new ClothesManagerUtils();
+    clothes.sort(sortType == SortType.ASC ? sorter.new SortClothesByPriceInUAH()
+        : sorter.new SortClothesByPriceInUAH().reversed());
+  }
+
+  public static void sortClothesBySize(List<AbstractClothes> clothes, SortType sortType) {
+    Comparator<AbstractClothes> comparator = new Comparator<AbstractClothes>() {
+
+      @Override
+      public int compare(AbstractClothes firstClothes, AbstractClothes secondClothes) {
+        return firstClothes.getSize() - secondClothes.getSize();
+      }
+    };
+    clothes.sort(sortType == SortType.ASC ? comparator : comparator.reversed());
+  }
+
+  public static void sortClothesByColor(List<AbstractClothes> clothes, SortType sortType) {
+    Comparator<AbstractClothes> comparator = (firstClothes, secondClothes) -> firstClothes.getColor()
+        .compareTo(secondClothes.getColor());
+    clothes.sort(sortType == SortType.ASC ? comparator : comparator.reversed());
+  }
+
+  static class SortClothesByBrands implements Comparator<AbstractClothes> {
+
+    @Override
+    public int compare(AbstractClothes firstClothes, AbstractClothes secondClothes) {
+      return firstClothes.getBrandName().compareTo(secondClothes.getBrandName());
+    }
 
   }
 
-  public static void sortClothesByPrice(List<AbstractClothes> clothes, SortType sortType) {
+  class SortClothesByPriceInUAH implements Comparator<AbstractClothes> {
 
+    @Override
+    public int compare(AbstractClothes fitstClothes, AbstractClothes secondClothes) {
+      return (int) (fitstClothes.getPriceInUAH() - secondClothes.getPriceInUAH());
+    }
   }
-} 
+}
