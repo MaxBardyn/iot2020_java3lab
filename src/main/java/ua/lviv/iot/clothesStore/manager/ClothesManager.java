8db4ca6bcd838d002b2e2e154d6925db75392af1
MaxBardyn <maxbardyn@gmail.com>
2020-03-31 16:20:41 +0300

    add junit test files

---
+++ src/main/java/ua/lviv/iot/clothesStore/manager/ClothesManager.java
@@ -0,0 +1,26 @@
+package ua.lviv.iot.clothesStore.manager;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import ua.lviv.iot.clothesStore.model.AbstractClothes;
+
+public class ClothesManager {
+
+  private List<AbstractClothes> clothes = new LinkedList<AbstractClothes>();
+
+  public void addClothes(List<AbstractClothes> clothes) {
+    this.clothes.addAll(clothes);
+  }
+
+  public List<AbstractClothes> findClothesForCeremonialReception() {
+
+    List<AbstractClothes> suitableClothes = new LinkedList<AbstractClothes>();
+    for (AbstractClothes currentClothes : clothes) {
+      if (currentClothes.isSuitableForCeremonialReception()) {
+        suitableClothes.add(currentClothes);
+      }
+    }
+    return suitableClothes;
+  }
+}
---
+++ src/test/java/ua/lviv/iot/clothesStore/manager/ClothesManagerUtilsTest.java
@@ -0,0 +1,19 @@
+package ua.lviv.iot.clothesStore.manager;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class ClothesManagerUtilsTest extends BaseClothesManager {
+
+	@Test
+	public void testSortClothesByBrands() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSortClothesByPrice() {
+    fail("Not yet implemented");
+  }
+
+}
